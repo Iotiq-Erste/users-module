ad8fa6d67b193b23899afb140552e48cc3e76a91
inci <inci@iotiq.net>
2023-10-13 15:54:27 +0300

    remove token validation from jwt filter

--- src/main/java/com/iotiq/user/security/jwt/JWTFilter.java
+++ src/main/java/com/iotiq/user/security/jwt/JWTFilter.java
@@ -31,7 +31,7 @@ public class JWTFilter extends GenericFilterBean {
             throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
         String jwt = resolveToken(httpServletRequest);
-        if (StringUtils.hasText(jwt) && this.tokenProvider.validateToken(jwt)) {
+        if (StringUtils.hasText(jwt)) {
             Authentication authentication = this.tokenProvider.getAuthentication(jwt);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
