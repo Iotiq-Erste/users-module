5c967e9cf00ef04a4ce848708185c71a7c45da7c
inci <inci@iotiq.net>
2023-10-17 09:23:01 +0300

    refactor JwtKeycloakAuthConverter

--- src/main/java/com/iotiq/user/security/jwt/JwtKeycloakAuthConverter.java
+++ src/main/java/com/iotiq/user/security/jwt/JwtKeycloakAuthConverter.java
@@ -17,29 +17,27 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtGra
 import org.springframework.stereotype.Component;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @Component
 @RequiredArgsConstructor
 public class JwtKeycloakAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
-    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter =
-            new JwtGrantedAuthoritiesConverter();
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
     private final UserService userService;
     @Value("${keycloak.auth.principle-attribute}")
     private String principleAttribute;
-    @Value("${keycloak.auth.resource-id}")
-    private String resourceId;
 
     @Override
     public AbstractAuthenticationToken convert(@NonNull Jwt jwt) {
-        Collection<GrantedAuthority> roles = Stream.concat(
-                jwtGrantedAuthoritiesConverter.convert(jwt).stream(),
-                extractRealmRoles(jwt).stream()
-        ).collect(Collectors.toSet());
+
+        Collection<GrantedAuthority> roles = jwtGrantedAuthoritiesConverter.convert(jwt);
+        roles.addAll(extractRealmRoles(jwt));
+
         User user = userService.findByUserName(getPrincipleClaimName(jwt));
         TransientUser principal = new TransientUser(user.getId(), user.getUsername(), "", roles);
 
@@ -51,46 +49,20 @@ public class JwtKeycloakAuthConverter implements Converter<Jwt, AbstractAuthenti
         if (principleAttribute != null) {
             claimName = principleAttribute;
         }
-        String userName = jwt.getClaim(claimName);
-        return userName;
+        return jwt.getClaim(claimName);
     }
 
     private Collection<? extends GrantedAuthority> extractRealmRoles(Jwt jwt) {
-        Map<String, Object> realmAccess;
-        Collection<String> realmRoles;
-
         if (jwt.getClaim("realm_access") == null) {
             return Set.of();
         }
 
-        realmAccess = (Map<String, Object>) jwt.getClaim("realm_access");
-        realmRoles = (Collection<String>) realmAccess.get("roles");
+        Map<String, List<String>> realmAccess = jwt.getClaim("realm_access");
+        List<String> realmRoles = realmAccess.get("roles");
 
         return realmRoles
                 .stream()
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
                 .collect(Collectors.toSet());
     }
-
-    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
-        Map<String, Object> resourceAccess;
-        Map<String, Object> resource;
-        Collection<String> resourceRoles;
-        if (jwt.getClaim("resource_access") == null) {
-            return Set.of();
-        }
-        resourceAccess = jwt.getClaim("resource_access");
-
-        if (resourceAccess.get(resourceId) == null) {
-            return Set.of();
-        }
-        resource = (Map<String, Object>) resourceAccess.get(resourceId);
-
-        resourceRoles = (Collection<String>) resource.get("roles");
-        return resourceRoles
-                .stream()
-                .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
-                .collect(Collectors.toSet());
-    }
-
 }
