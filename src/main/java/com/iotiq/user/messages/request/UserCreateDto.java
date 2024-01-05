276588917dcd42936b7c26b83827ce239e559cf5
YusufZartit <gonenc@dakikyazilim.com>
2024-01-05 13:19:48 +0300

    Refactor 'UserService' to Enable Modification of Keycloak User Information.

--- src/main/java/com/iotiq/user/messages/request/UserCreateDto.java
+++ src/main/java/com/iotiq/user/messages/request/UserCreateDto.java
@@ -10,6 +10,9 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
 public class UserCreateDto extends UserUpdateDto {
+    @NotEmpty(message = "user.username")
+    @Size(min = 1, max = 50, message = "user.usernameSize")
+    private String username;
     @NotEmpty(message = "user.password")
     @Size(min = 4, max = 100, message = "user.passwordSize")
     private String password;
