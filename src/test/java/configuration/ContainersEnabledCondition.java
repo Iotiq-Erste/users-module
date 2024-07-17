27eeb571fbd02a91c276b63c5ef044f3ff84cef6
mesut <mesut@iotiq.net>
2024-07-17 16:33:54 +0300

    feat(ci-cd): Add ContainersEnabledCondition to provide k8s pipelines.

---
+++ src/test/java/configuration/ContainersEnabledCondition.java
@@ -0,0 +1,15 @@
+package configuration;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+public class ContainersEnabledCondition implements Condition {
+
+    @Override
+    public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+        String testContainersDisabled = context.getEnvironment().getProperty("TEST_CONTAINERS_DISABLED");
+        return testContainersDisabled == null || !Boolean.parseBoolean(testContainersDisabled);
+    }
+
+}
